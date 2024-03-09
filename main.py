77ea00d7be442d330ad1ee4744ad5640cb03969c
Core447 <jmulmann@protonmail.com>
2024-03-09 13:50:33 +0100

    Update github urls to fit organization

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ class ClocksPlugin(PluginBase):
         # Register plugin
         self.register(
             plugin_name=self.lm.get("plugin.name"),
-            github_repo="https://github.com/Core447/Clocks",
+            github_repo="https://github.com/StreamController/Clocks",
             plugin_version="1.0.0",
             app_version="1.0.0-alpha"
         )
