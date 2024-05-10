24a9a99cb977ed89cf1239a5c1269ec66a9b8c9c
Core447 <jmulmann@protonmail.com>
2024-05-10 17:33:00 +0200

    Update constructors

--- main.py
+++ main.py
@@ -29,10 +29,8 @@ from src.backend.PageManagement.Page import Page
 from AnalogClockGenerator import AnalogClockGenerator
 
 class AnalogClock(ActionBase):
-    def __init__(self, action_id: str, action_name: str,
-                 deck_controller: "DeckController", page: Page, coords: str, plugin_base: PluginBase):
-        super().__init__(action_id=action_id, action_name=action_name,
-            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
         self.generator = AnalogClockGenerator(
             hour_markings_width=9,
@@ -54,10 +52,8 @@ class AnalogClock(ActionBase):
 
 
 class DigitalClock(ActionBase):
-    def __init__(self, action_id: str, action_name: str,
-                 deck_controller: "DeckController", page: Page, coords: str, plugin_base: PluginBase):
-        super().__init__(action_id=action_id, action_name=action_name,
-            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
         self.HAS_CONFIGURATION = True
         
